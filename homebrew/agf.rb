4c4b2d683bcd694087b2c755469a11fed560f12c
Subin An <subinium@gmail.com>
2026-03-20 01:35:37 +0900

    chore: update homebrew formula to v0.6.4

--- homebrew/agf.rb
+++ homebrew/agf.rb
@@ -7,16 +7,16 @@ class Agf < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/subinium/agf/releases/download/v#{version}/agf-aarch64-apple-darwin.tar.gz"
-      sha256 "7a70a7a47e2abb0adc45d5eb23dcddb8aeb3d46845069888fc994661ffa0e6ca"
+      sha256 "ec8e9f1621c77053c2e1adbade3ee4ccb58fd349936aee2ab9748b47fa3ba421"
     else
       url "https://github.com/subinium/agf/releases/download/v#{version}/agf-x86_64-apple-darwin.tar.gz"
-      sha256 "d626ab1178585fcdfb496b850c0923c6d8168ea27c5e4133eacafb3de906496e"
+      sha256 "fe08cda8c95ca660547c8cc53f7c3ee79844793ee12d81a1002d341c9fee67fd"
     end
   end
 
   on_linux do
     url "https://github.com/subinium/agf/releases/download/v#{version}/agf-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "c67ef9bd3c630ddf410813cf5290febe03c716da6422511ee8f9be44e9c652a7"
+    sha256 "0dac29453b0cdf1959fed53b80efe77138db24cb3c1982fde5507a88388679a7"
   end
 
   def install
